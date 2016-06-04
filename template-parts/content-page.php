8814bd34525872478866b3b036cbc980b1f041e6
Anthony Skelton <anthony@redcartstudios.com>
2016-06-03 18:11:13 -0700

    Added conditional for if front page to not show the title of the page

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -10,9 +10,14 @@
 ?>
 
 <article <?php post_class(); ?>>
+	<?php if (!is_front_page()) :
+
+	?>
 	<header class="entry-header">
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 	</header><!-- .entry-header -->
+	<?php endif; ?>
+
 
 	<div class="entry-content">
 		<?php
