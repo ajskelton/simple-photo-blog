2084038b182e596f4f475057ce6482cd10dd511a
Anthony Skelton <anthony@redcartstudios.com>
2016-06-13 03:25:57 -0700

    Moved content from template-part back into single template, removed is_single conditionals

--- single.php
+++ single.php
@@ -14,9 +14,47 @@ get_header(); ?>
 			<main id="main" class="site-main" role="main">
 
 			<?php
-			while ( have_posts() ) : the_post();
+			while ( have_posts() ) : the_post(); ?>
 
-				get_template_part( 'template-parts/content', get_post_format() );
+				<article <?php post_class(); ?>>
+					<?php 
+						if(has_post_thumbnail()){
+							ajs_spb_do_post_image( $size = 'full' );
+						}
+					?>
+					<?php
+						the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+					?>
+					<div class="entry-content">
+						
+						<div class="entry-description">
+							<?php ajs_spb_posted_on(); ?>
+						
+							<?php
+								the_content( sprintf(
+									/* translators: %s: Name of current post. */
+									wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', '_s' ), array( 'span' => array( 'class' => array() ) ) ),
+									the_title( '<span class="screen-reader-text">"', '"</span>', false )
+								) );
+								wp_link_pages( array(
+									'before' => '<div class="page-links">' . esc_html__( 'Pages:', '_s' ),
+									'after'  => '</div>',
+								) );
+							?>
+						
+							<div class="image-meta">
+								<?php ajs_spb_get_exif_info(); ?>
+							</div>
+						</div><!-- .entry-description -->
+
+					</div><!-- .entry-content -->
+
+					<footer class="entry-footer">
+						<?php ajs_spb_entry_footer(); ?>
+					</footer><!-- .entry-footer -->
+					<hr>
+				</article><!-- #post-## -->
+				<?php
 
 				// the_post_navigation();
 				ajs_spb_thumbnail_navigation();
