821363847af98ed63e84ae4fdd7d21b76f09bea0
Anthony Skelton <anthony@redcartstudios.com>
2016-06-21 12:25:17 -0700

    Moved queries to template tags and added conditionals for these tags based on customizer settings

--- front-page.php
+++ front-page.php
@@ -15,50 +15,18 @@
 get_header(); ?>
 
 	<div class="wrap">
+	<?php 
+		if( get_theme_mod( 'ajs_spb_most_recent_post' ) ){
+			ajs_spb_front_page_most_recent_post();
+		}
+	?>
 		<div class="primary content-area">
 			<main id="main" class="site-main" role="main">
 				<div class="most-recent-post">
-				<?php
-					$args = array( 
-						'numberposts' => '31',
-						'post_status' => 'publish',
-						'tax_query' => array(
-								array(
-									'taxonomy' => 'post_format',
-									'field' => 'slug',
-									'terms' => 'post-format-image',
-									'operator' => 'IN'
-								),
-						) );
-						$recent_posts = wp_get_recent_posts( $args );
-						$first = true;
-						$index_grid = [];
-						foreach( $recent_posts as $recent ){
-							if($first == true) {
-								$featured_image = get_the_post_thumbnail( $recent['ID'], 'full' );
-								echo '<a href="'.$recent['guid'].'">';
-								echo $featured_image;
-								echo '</a>';
-								echo '<div class="entry-content">';
-								echo '<h2 class="entry-title"><a href="'.$recent['guid'].'">'.$recent['post_title'].'</a></h2>';
-								ajs_spb_posted_on();
-								echo '<p class="entry-description">'.$recent['post_excerpt'].'</p>';
-								echo '</div> <!-- .entry-content -->';
-								echo '<hr>';
-							} else {
-								array_push($index_grid, array(get_the_post_thumbnail( $recent['ID'], 'thumbnail' ), $recent['guid'] ) );
-							}
-							$first = false;
-						}
-				?>
+				
 				</div>
 
-				<?php
-				while ( have_posts() ) : the_post();
-
-					if( ! is_front_page() && has_post_thumbnail() ) {
-						ajs_spb_do_post_image( $size = 'full' );
-					} ?>
+				<?php while ( have_posts() ) : the_post(); ?>
 
 					<div class="front-page-content">
 						<?php
@@ -71,23 +39,13 @@ get_header(); ?>
 						?>
 					</div><!-- .front-page-content -->
 
-				<?php
-
-				endwhile; // End of the loop.
+				<?php endwhile; // End of the loop. ?>
 				
-				?>
-				<h2>Recent Photos</h2>
-				<section id="other-recent-posts">
-				<?php
-					foreach( $index_grid as $grid_item){
-						echo '<div class="index-grid">';
-						echo '<a href="'.$grid_item[1].'">';
-						echo $grid_item[0];
-						echo '</a>';
-						echo '</div>';
+				<?php 
+					if( get_theme_mod( 'ajs_spb_index_grid_enable' ) ) {
+						ajs_spb_front_page_index_grid();
 					}
-				?>
-				</section><!-- #other-recent-posts -->
+			 	?>
 
 			</main><!-- #main -->
 		</div><!-- .primary -->
