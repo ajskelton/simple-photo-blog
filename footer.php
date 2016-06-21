b4fd4e26711c5b46dcbfa2507e60f7bbf2c19470
Anthony Skelton <anthony@redcartstudios.com>
2016-06-21 12:51:03 -0700

    Added credits section

--- footer.php
+++ footer.php
@@ -19,6 +19,12 @@
 			<div class="site-info">
 				<?php ajs_spb_do_copyright_text(); ?>
 			</div>
+			<div class="credits">
+				<span>Proudly powered by </span>
+				<a href="http://www.wordpress.org">WordPress</a>
+				<span> / Theme: </span>
+				<a href="http://anthonyskelton.com/simplephotoblog">Simple Photo Blog</a> by <a href="http://anthonyskelton.com">Anthony Skelton</a>
+			</div>
 
 		</div><!-- .wrap -->
 	</footer><!-- .site-footer -->
