b21eaa8f2069405bc80f6463eaecc1c2fbd42212
Anthony Skelton <anthony@redcartstudios.com>
2016-06-21 22:38:52 -0700

    Added social icon links with theme_mod checks

--- footer.php
+++ footer.php
@@ -10,11 +10,35 @@
  */
 
 ?>
-
 	</div><!-- #content -->
 
 	<footer class="site-footer">
 		<div class="wrap">
+		<?php if( get_theme_mod( 'ajs_spb_social_enable' ) ) { ?>
+			<ul class="social-icons">
+				<?php if( get_theme_mod( 'ajs_spb_facebook_link' ) ) : ?>
+				<li class="social-icon">
+					<a href="<?php echo get_theme_mod( 'ajs_spb_facebook_link' ); ?>">
+						<?php ajs_spb_do_svg( $args = array( 'icon' => 'facebook-square') ); ?>
+					</a>
+				</li>
+				<?php endif; ?>
+				<?php if( get_theme_mod( 'ajs_spb_twitter_link' ) ) : ?>
+				<li class="social-icon">
+					<a href="<?php echo get_theme_mod( 'ajs_spb_twitter_link' ) ?>">
+						<?php ajs_spb_do_svg( $args = array( 'icon' => 'twitter-square' ) ); ?>
+					</a>
+				</li>
+				<?php endif; ?>
+				<?php if( get_theme_mod( 'ajs_spb_instagram_link' ) ) : ?>
+				<li class="social-icon">
+					<a href="<?php echo get_theme_mod( 'ajs_spb_instagram_link' ) ?>">
+						<?php ajs_spb_do_svg( $args = array( 'icon' => 'instagram' ) ); ?>
+					</a>
+				</li>
+				<?php endif; ?>
+			</ul>
+		<?php } ?>
 
 			<div class="site-info">
 				<?php ajs_spb_do_copyright_text(); ?>
