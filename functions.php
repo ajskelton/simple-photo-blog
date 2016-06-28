dd151131e5df6fb1df21cedbe17c59c8c4038665
Anthony Skelton <anthony@redcartstudios.com>
2016-06-28 15:03:02 -0700

    Removed custom header for now

--- functions.php
+++ functions.php
@@ -151,7 +151,7 @@ function ajs_spb_convert_decimal_to_fraction($imagemeta) {
 /**
  * Implement the Custom Header feature.
  */
-require get_template_directory() . '/inc/custom-header.php';
+//require get_template_directory() . '/inc/custom-header.php';
 
 /**
  * Custom template tags for this theme.
