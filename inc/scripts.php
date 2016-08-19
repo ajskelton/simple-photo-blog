7af8942aa0d0c1ec5920a4b0508bcae99eec941b
Anthony Skelton <anthony@redcartstudios.com>
2016-08-19 15:53:23 -0700

    Added Customizer.js file

--- inc/scripts.php
+++ inc/scripts.php
@@ -72,6 +72,7 @@ function ajs_spb_scripts() {
 
 	// Enqueue scripts.
 	wp_enqueue_script( 'ajs_spb-scripts', get_template_directory_uri() . '/assets/js/project' . $suffix . '.js', array( 'jquery' ), $version, true );
+	wp_enqueue_script( 'ajs_spb-customizer', get_template_directory_uri() . '/assets/js/customizer.js', array( 'jquery' ), $version, true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
