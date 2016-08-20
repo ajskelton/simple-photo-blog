2aa28e9646c0f3bc8273b7566261b5b79c785080
Anthony Skelton <anthony@redcartstudios.com>
2016-08-19 17:31:40 -0700

    Moved enqueuing of customizer.js script to the customize_preview_init hook

--- inc/scripts.php
+++ inc/scripts.php
@@ -72,13 +72,25 @@ function ajs_spb_scripts() {
 
 	// Enqueue scripts.
 	wp_enqueue_script( 'ajs_spb-scripts', get_template_directory_uri() . '/assets/js/project' . $suffix . '.js', array( 'jquery' ), $version, true );
-	wp_enqueue_script( 'ajs_spb-customizer', get_template_directory_uri() . '/assets/js/customizer.js', array( 'jquery' ), $version, true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
 }
 add_action( 'wp_enqueue_scripts', 'ajs_spb_scripts' );
+
+
+/*
+* Enqueue WordPress Customizer Script for Live Updating
+*
+* @since 1.0.0
+ */
+function ajs_spb_customizer_live_preview()
+{
+	wp_enqueue_script( 'ajs_spb-customizer', get_template_directory_uri().'/assets/js/customizer.js', array( 'jquery','customize-preview' ), '1.0.0', true );
+}
+add_action( 'customize_preview_init', 'ajs_spb_customizer_live_preview' );
+
 
 
 if ( class_exists( 'WDS_Simple_Page_Builder' ) && version_compare( WDS_Simple_Page_Builder::VERSION, '1.6', '>=' ) ) :
